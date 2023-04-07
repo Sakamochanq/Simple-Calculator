902e6568f30718df497319cce4cad5383e43015f
Sakamochanq <126952815+Sakamochanq@users.noreply.github.com>
2023-04-07 22:55:58 +0900

    Added Simple Calclator Source

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+# include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+    printf("[ Simple Calculator v1.0 ]\n");
+
+    char Selc;
+
+    float num1, num2;
+
+    cout << "Enter the Options : [+], [-], [*], [/]" << "\n";
+    cin >> Selc;
+
+    cout << "Enter the two Operandrs : " << "\n";
+    cin >> num1 >> num2 ;
+
+    switch (Selc)
+    {
+        case '+' /* constant-expression */:
+            cout << num1 << " + " << num2 << " = " << num1+num2 << endl;
+            break;
+
+        case '-' /* constant-expression */:
+            cout << num1 << " - " << num2 << " = " << num1-num2 << endl;
+            break;
+
+        case '*' /* constant-expression */:
+            cout << num1 << " * " << num2 << " = " << num1*num2 << endl;
+            break;
+
+        case '/' /* constant-expression */:
+            cout << num1 << " / " << num2 << " = " << num1/num2 << endl;
+            break;
+    
+    default:
+
+        cout << "Error !";
+    }
+
+    system("pause");
+
+    return 0;
+}
