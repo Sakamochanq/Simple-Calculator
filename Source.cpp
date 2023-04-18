4439b4b9325e7fb57783a8235db2d0a2dd925458
Korozin <90534409+Korozin@users.noreply.github.com>
2023-04-18 11:24:49 -0400

    Optimize

--- Source.cpp
+++ Source.cpp
@@ -1,67 +1,29 @@
-#include <stdio.h>
 #include <iostream>
-
-int num1, num2, result;
-
-char op;
+#include <cmath>
 
 using namespace std;
 
 int main() {
+    double num1, num2;
+    char op;
 
-    printf("[  Simple Calculator v1.2  ] \n");
+    cout << "Enter equation: ";
+    cin >> num1 >> op >> num2;
 
-    std::cout << "> ";
-    std::cin >> num1;
+    double result = (op == '+') ? num1 + num2 :
+                    (op == '-') ? num1 - num2 :
+                    (op == '*') ? num1 * num2 :
+                    (op == '/') ? num1 / num2 :
+                    (op == '^') ? pow(num1, num2) :
+                    NAN;
 
-    std::cout << "[+] [-] [*] [/] > ";
-    std::cin >> op;
-
-    std::cout << "> ";
-    std::cin >> num2;
-
-    if (op == '+')
-    {
-        result = num1 + num2;
-
-        std::cout << num1 << " + " << num2 << " = " << result << endl;
-
-        system("pause");
-    }
-
-    else if (op == '-')
-    {
-        result = num1 - num2;
-
-        std::cout << num1 << " - " << num2 << " = " << result << endl;
+    if (isnan(result)) {
+        cout << "Invalid operator" << endl;
+    } else {
+        cout << num1 << " " << op << " " << num2 << " = " << result << endl;
 
         system("pause");
     }
 
-    else if (op == '*')
-    {
-        result = num1 * num2;
-
-        std::cout << num1 << " + " << num2 << " = " << result << endl;
-
-        system("pause");
-    }
-
-    else if (op == '/')
-    {
-        result = num1 / num2;
-
-        std::cout << num1 << " / " << num2 << " = " << result << endl;
-
-        system("pause");
-    }
-
-    else 
-    {
-        std::cout << "Error" << endl;
-
-        system("pause");
-
-        return 0;
-    }
+    return 0;
 }
