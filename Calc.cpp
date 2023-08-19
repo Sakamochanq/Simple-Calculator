5403b6341be2ab2d3ffe0961cb1bce9e08b87b17
Khaled Gadelhaq <120754711+khaledGadelhaQ@users.noreply.github.com>
2023-08-19 15:04:49 +0300

    Adding documentation.
    Explaining how the code works :)

--- Calc.cpp
+++ Calc.cpp
@@ -6,17 +6,25 @@ using namespace std;
 int main() {
     double num1, num2;
     char op;
-
+  
     cout << "Enter equation: ";
-    cin >> num1 >> op >> num2;
-
+    //// reading a number followed by a char followed by another number from the user
+    cin >> num1 >> op >> num2; 
+    //// result computes the math operation that the user has chosen (op)
+    
+    
     double result = (op == '+') ? num1 + num2 :
                     (op == '-') ? num1 - num2 :
                     (op == '*') ? num1 * num2 :
                     (op == '/') ? num1 / num2 :
                     (op == '^') ? pow(num1, num2) :
                     NAN;
-
+   //// If it is not in the previous possible choices it prints ("Invalid operator")
+    /*
+    In C++, the isnan function is used to determine whether a given floating-point value is a "Not-a-Number" 
+    (NaN) value or not. NaN is a special value that represents the result of undefined or indeterminate mathematical operations, 
+    such as dividing zero by zero or taking the square root of a negative number.
+    */
     if (isnan(result)) {
         cout << "Invalid operator" << endl;
     } else {
